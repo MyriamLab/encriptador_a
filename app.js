b5cd983407cdcb68309c0a3a9bdb488d78a465fa
Myriam <myriam.rivera.santiago@gmail.com>
2022-09-04 20:55:43 -0500

    se agrega carpeta test

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
 const textarea = document.getElementById("js_textarea");
 const mostrarTxtEncriptado = document.getElementById("js_vista2-TxtEncriptado");
 
-
-function validar(mensaje){
-    let spanMensaje = document.getElementById("spanMensaje");
-    let filtroUno = mensaje
-    console.log(mensaje)
-    
-    let letras = "abcdefghijklmopqrstuvwzy";
-    if(!filtroUno.includes(letras)){ //si el mensaje no incluye letras 
-        //console.log("Ingresa sólo caracteres simples")
-        spanMensaje.innerHTML ="Ingresa sólo caracteres simples";
-        spanMensaje = "";
-    }else {
-        console.log("error")
-        //encriptar()
-    }
-}
-
-
 function encriptar(){
     let mensajeAencriptar = textarea.value;
     mensajeAencriptar = mensajeAencriptar.toLowerCase();
@@ -35,12 +17,8 @@ function encriptar(){
 
     // PASO EL MENSAJE ENCRIPTADO AL VALOR DEL SPAN QUE SE MUESTA EN LA SECCIÓN 2 
     mostrarTxtEncriptado.value = mensajeEncriptado;
-  
-   // console.log( mostrarTxtEncriptado.value)    
-  validar (textarea.value)
-   //aquí llamo a la función validar con un parámetro que recibe como argumento el valor de mensajeAencriptar
+    // console.log( mostrarTxtEncriptado.value)    
 }
-
 
 function desEncriptar(){
     let mensajeEncriptado =  mostrarTxtEncriptado.value;
@@ -52,6 +30,7 @@ function desEncriptar(){
     .replaceAll("ufat", "u");
 
     mostrarTxtEncriptado.value = mensajeDesEncriptado;
+    spanMensaje.value = "";
     console.log(mensajeEncriptado);
 }
 
@@ -61,10 +40,8 @@ function copiar(){
     console.log( mensajeEncriptado)
     navigator.clipboard.writeText(mensajeEncriptado.value);
     textarea.value=""; //limpia el área del mensaje a encriptar 
-    textarea.focus();
+    textarea.focus(); 
 }
-
-
 
 //BOTÓN ENCRIPTAR
 const botonEncriptar = document.getElementById("js_buttonEncrypt");
@@ -87,4 +64,19 @@ function cambiarPantalla(){
    
 }
 
+module.exports =  { encriptar, desEncriptar, copiar }; 
 
+/*
+function validar(mensaje){
+    let spanMensaje = document.getElementById("spanMensaje");
+    let filtroUno = mensaje
+    console.log(mensaje)
+    
+    let letras = "abcdefghijklmopqrstuvwzy";
+   if(!filtroUno.includes(letras)){ //si el mensaje no incluye letras 
+        spanMensaje.innerHTML ="Ingresa sólo caracteres simples";
+        spanMensaje.value = "";
+   }
+        //console.log("Ingresa sólo caracteres simples")
+   
+}*/
